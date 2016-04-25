2b6cf95e2ff6ede788f1c78ff7a131bf1e3c6168
Malin <malin_nilsson@hotmail.com>
2016-04-25 21:40:38 +0200

    bakblogg
    tried adding custom top nav but nothing comes out as of now..

--- wp-content/themes/twentysixteen-bakblogg/functions.php
+++ wp-content/themes/twentysixteen-bakblogg/functions.php
@@ -9,5 +9,19 @@ function theme_enqueue_styles() {
         array( $parent_style )
     );
 }
+
+function queu_bootstrap()
+{
+    wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri().'/bootstrap/js/bootstrap.min.js', array('jquery'), NULL, true);
+    wp_enqueue_style('bootstrap-css', get_stylesheet_directory_uri().'/bootstrap/css/bootstrap.min.css', false, NULL, 'all');
+}
+
+function register_top_menu() {
+  register_nav_menu('bakblogg-top-menu',__( 'Bakblogg Top Menu' ));
+}
+
+add_action( 'init', 'register_top_menu' );
+add_action('wp_enqueue_scripts', 'queu_bootstrap', PHP_INT_MAX);
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
 ?>
